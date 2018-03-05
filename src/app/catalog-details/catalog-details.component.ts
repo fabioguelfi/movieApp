a3c6c20328a42a57d5964906d494b94f577f3a05
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-03-04 23:25:48 -0300

    finalizar o refactory porém está funcional já

--- src/app/catalog-details/catalog-details.component.ts
+++ src/app/catalog-details/catalog-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from "@angular/common/http";
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'mt-catalog-details',
@@ -8,11 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CatalogDetailsComponent implements OnInit {
   public idMovie;
-  constructor(private route: ActivatedRoute) { }
+
+  movieDetails: Object;
+
+  constructor(private route: ActivatedRoute, private movieService: MovieService) { }
 
   ngOnInit() {
     let id  = this.route.snapshot.params['id'];
     this.idMovie = id;
+
+    this.movieService.getMovieById(this.idMovie).subscribe(movieListDetails => {
+      this.movieDetails = movieListDetails;
+    })
   }
 
 }
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -7,14 +7,20 @@ import 'rxjs/add/operator/map';
 export class MovieService {
   constructor(private http: HttpClient) {}
 
+  private ROOT_URL_MOVIE: string = "https://api.themoviedb.org/3/movie/";
   private ROOT_URL_GENRE: string = "https://api.themoviedb.org/3/genre/";
   private API_KEY:string = "?api_key=33092a849275dfe342ef839a9f14ddb9";
-  private GENRE_NUMBER:string = "878/movies"; /*popular com uma func que da a opcao ao usuario escolher o genero*/
+  private GENRE_NUMBER:string = "35/movies";
+  /*popular com uma func que da a opcao ao usuario escolher o genero*/
   private LANGUAGE:string = "&language=pt-BR";
 
   getMovies() {
     return this.http.get(this.ROOT_URL_GENRE + this.GENRE_NUMBER + this.API_KEY + this.LANGUAGE)
     .map(res => res)
+  }
+
+  getMovieById(id: any) {
+    return this.http.get(this.ROOT_URL_MOVIE + id + this.API_KEY + this.LANGUAGE)
   }
 
 }
